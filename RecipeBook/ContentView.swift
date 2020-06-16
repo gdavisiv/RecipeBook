87efe6d77544a6c689b8248ab1efdb5812e36cb1
George Davis IV <GDavisIV@gmail.com>
2020-06-16 09:16:10 -0400

    Initial Commit

---
+++ RecipeBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeBook
+//
+//  Created by George Davis IV on 6/16/20.
+//  Copyright © 2020 3DMations. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
