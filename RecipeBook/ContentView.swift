190ab74f8e733bec4a02512cf54f51cd36c07a66
George Davis IV <GDavisIV@gmail.com>
2020-06-17 14:40:53 -0400

    Creating Data Array for images/info/etc

--- RecipeBook/ContentView.swift
+++ RecipeBook/ContentView.swift
@@ -104,7 +104,7 @@ struct Home : View {
                     .clipShape(Capsule())
                     
                 }
-                .padding(.top, 25)
+                .padding(.top, 30)
                 
                 Spacer()
                 
@@ -113,3 +113,67 @@ struct Home : View {
         }
     }
 }
+
+struct List : View {
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(data) { i in
+                GeometryReader{g in
+                    Card(width: g.frame(in: .global).width, data: i)
+                }
+            }
+        }
+    }
+}
+
+struct Card : View {
+    
+    var width : CGFloat
+    var data : Type
+    
+    var body: some View {
+        VStack {
+            Text(self.data.name)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top, 20)
+            
+            Text(self.data.cName)
+                .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                .padding(.vertical)
+            
+            Image(self.data.image)
+                .resizable()
+                .frame(width: self.width - 100, height: 250)
+                .cornerRadius(20)
+            
+            Text(self.data.ingredients)
+                .fontWeight(.light)
+                .font(.caption)
+                .padding(.top, 20)
+            
+            Button(action: {
+                
+            }) {
+                Text("Save")
+            }
+        }
+    }
+}
+
+struct Type : Identifiable {
+    
+    var id : Int
+    var name : String
+    var cName : String
+    var ingredients : String
+    var image : String
+    
+}
+
+var data = [
+    Type(id: 0, name: "Quinoa", cName: "Soul Food", ingredients: "Quinoa, White Rice, salt, pepper, etc", image: "Quinoa"),
+    Type(id: 1, name: "Steak", cName: "Soul Food", ingredients: "Steak, salt, pepper, etc", image: "Quinoa"),
+    
+
+]
